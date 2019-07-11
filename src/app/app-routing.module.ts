2a63043c2afcc3e388d6c34f7c188899e66ae6be
System Administrator <root@MacBook-Air-de-Adbid.local>
2019-07-11 16:50:06 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: 'detail', component: DetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
